29d499c968129a3060ab85cd45a4589bd71f821b
Sujitshetty <sujitshetty@gmail.com>
2017-11-07 14:45:55 +0530

    udpated url for mongo

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://sujitsmongo:aevTU7B7N7nU34DAshNHZa6RlUdWGLJxCFHx0vHDfVfdXJ9oqjnyqD19CMaZXY1pEpwzeMwsa776u5x9Fxlnxw==@sujitsmongo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
